a6d62cc76a7d08b14c3a8b371c30bdc5378fa584
yangfan <yangfan_web@163.com>
2017-06-11 22:01:22 +0800

    ___a

--- leetcode/1 Two Sum/twoSum.js
+++ leetcode/1 Two Sum/twoSum.js
@@ -1,10 +1,10 @@
-var twoSum = function(nums, aa) {
+var twoSum = function(nums, ___a) {
     var temp = nums.slice(0); //数组拷贝
     nums = nums.sort(function(a, b) { return a - b; }); //数组从小到大排序
     var i = 0;
     var j = nums.length - 1;
-    while (nums[i] + nums[j] != aa) { //相加为target的时候退出循环
-        if (nums[i] + nums[j] > aa) {
+    while (nums[i] + nums[j] != ___a) { //相加为target的时候退出循环
+        if (nums[i] + nums[j] >___a) {
             j--;
         } else {
             i++;
